9a6f8ca2fea4065e93dc5b95cf22af8af9f5169f
Double L <1138494584@qq.com>
2016-10-10 20:58:33 +0800

    修改配置上的错误

--- src/com/violentstone/service/ReplyService/ReplyService.java
+++ src/com/violentstone/service/ReplyService/ReplyService.java
@@ -3,44 +3,47 @@ package com.violentstone.service.ReplyService;
 import java.util.List;
 
 import com.violentstone.Interface.IReply;
+import com.violentstone.dao.replyDao.ReplyImpl;
 import com.violentstone.entity.reply.Reply;
 
 public class ReplyService implements IReply{
 
+	ReplyImpl RI = new ReplyImpl();
+	
 	@Override
 	public void addReply(Reply reply) {
 		// TODO Auto-generated method stub
-		
+		RI.addReply(reply);
 	}
 
 	@Override
 	public void delReplyByRly(int rlyId) {
 		// TODO Auto-generated method stub
-		
+		RI.delReplyByRly(rlyId);
 	}
 
 	@Override
 	public void delReplyByCom(int comId) {
 		// TODO Auto-generated method stub
-		
+		RI.delReplyByCom(comId);
 	}
 
 	@Override
 	public void updateReply(Reply reply) {
 		// TODO Auto-generated method stub
-		
+		RI.updateReply(reply);
 	}
 
 	@Override
 	public Reply queryReplyByRly(int rlyId) {
 		// TODO Auto-generated method stub
-		return null;
+		return RI.queryReplyByRly(rlyId);
 	}
 
 	@Override
 	public List<Reply> queryReplyByCom(int comId) {
 		// TODO Auto-generated method stub
-		return null;
+		return RI.queryReplyByCom(comId);
 	}
 
 }
--- src/com/violentstone/servlet/ReplyServlet/QueryReplyByRlyServlet.java
+++ src/com/violentstone/servlet/ReplyServlet/QueryReplyByRlyServlet.java
@@ -8,10 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-
-
-
 import net.sf.json.JSONObject;
 
 import com.violentstone.entity.reply.Reply;
